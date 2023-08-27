2667483692d6724c6657c67aa5cc9a082b2225ac
tanghan <2824694660@qq.com>
2023-08-27 15:37:32 +0800

    main函数初始化kafka writer

--- user/main.go
+++ user/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"common/ggConfig"
 	"common/ggShutDown"
+	"user/pkg/utils"
 	"user/router"
 )
 
@@ -14,8 +15,17 @@ func main() {
 	// 将grpc服务注册到etcd
 	router.RegisterEtcdServer()
 
+	// 初始化kafka
+	kafkaCloseFunc := utils.InitKafkaWriter()
+
+	// 优雅启停的时候，将grpc服务一起停掉
+	stop := func() {
+		gc.Stop()
+		kafkaCloseFunc()
+	}
+
 	var exit = make(chan struct{})
 	<-exit
 	// todo 待完善功能
-	ggShutDown.ShutDown(userServerConfig.Name, userServerConfig.Addr, gc.Stop)
+	ggShutDown.ShutDown(userServerConfig.Name, userServerConfig.Addr, stop)
 }
